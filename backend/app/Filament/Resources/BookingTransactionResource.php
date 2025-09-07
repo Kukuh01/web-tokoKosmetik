e823ac21034da0a1c4a33d79eecd6b4d39df7853
Silvanus <silvanusprasetyo@gmail.com>
2025-09-07 17:08:12 +0700

    ADD: do steps booking and more

--- backend/app/Filament/Resources/BookingTransactionResource.php
+++ backend/app/Filament/Resources/BookingTransactionResource.php
@@ -16,9 +16,12 @@ use App\Filament\Resources\BookingTransactionResource\Pages;
 use App\Filament\Resources\BookingTransactionResource\RelationManagers;
 use App\Models\Cosmetic;
 use Dom\Text;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 
 class BookingTransactionResource extends Resource
 {
@@ -30,7 +33,7 @@ class BookingTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                //Komponen tambahan (plugin) di FilamentPHP yang memungkinkan kamu membuat form multi-step (bertingkat), seperti wizard atau langkah demi langkah (stepper).
                 Forms\Components\Wizard::make([
 
                     Forms\Components\Wizard\Step::make('Product and Price')
@@ -101,6 +104,72 @@ class BookingTransactionResource extends Resource
                             ->label('Total Tax (11%)'),
                         ]),
 
+                    ]),
+
+                    Forms\Components\Wizard\Step::make('Customer Information')
+                    ->completedIcon('heroicon-m-hand-thumb-up')
+                    ->description('For our marketing')
+                    ->schema([
+                        Grid::make(2)
+                        ->schema([
+                            TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+
+                            TextInput::make('phone')
+                            ->required()
+                            ->maxLength(255),
+
+                            TextInput::make('email')
+                            ->required()
+                            ->maxLength(255),
+                        ])
+                    ]),
+
+                    Forms\Components\Wizard\Step::make('Delivery Information')
+                    ->completedIcon('heroicon-m-hand-thumb-up')
+                    ->description('Put your correct address')
+                    ->schema([
+                        Grid::make(2)
+                        ->schema([
+                            TextInput::make('city')
+                            ->required()
+                            ->maxLength(255),
+
+                            TextInput::make('post_code')
+                            ->required()
+                            ->maxLength(255),
+
+                            Textarea::make('address')
+                            ->required()
+                            ->maxLength(255),
+                        ])
+                    ]),
+
+                    Forms\Components\Wizard\Step::make('Payment Information')
+                    ->completedIcon('heroicon-m-hand-thumb-up')
+                    ->description('Review your payment')
+                    ->schema([
+                        Grid::make(3)
+                        ->schema([
+                            TextInput::make('booking_trx_id')
+                            ->required()
+                            ->maxLength(255),
+
+                            ToggleButtons::make('is_paid')
+                            ->label('Apakah sudah membayar?')
+                            ->boolean()
+                            ->grouped()
+                            ->icons([
+                                true => 'heroicon-o-pencil',
+                                false => 'heroicon-o-clock',
+                            ])
+                            ->required(),
+
+                            FileUpload::make('proof')
+                            ->image()
+                            ->required(),
+                        ])
                     ])
 
                 ])
