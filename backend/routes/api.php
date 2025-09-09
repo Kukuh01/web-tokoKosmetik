9989ac480adb99f1a2198ddf7295b11e3d8b9013
Silvanus <silvanusprasetyo@gmail.com>
2025-09-09 14:46:29 +0700

    PRE: Preparation routing endpoint API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\BookingTransactionController;
+use App\Http\Controllers\Api\BrandController;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CosmeticController;
+use App\Models\Category;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cosmetic/{cosmetic:slug}', [CosmeticController::class, 'show']); // use binding model but replace the id to slug
+Route::apiResource('/cosmetics', CosmeticController::class); //register method crud laravel (index, show, edit, delete, etc...)
+
+Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
+Route::apiResource('/categories', CategoryController::class);
+
+Route::get('/brand/{brand:slug}', [BrandController::class, 'show']);
+Route::apiResource('/brands', BrandController::class);
+
+Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
+Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
